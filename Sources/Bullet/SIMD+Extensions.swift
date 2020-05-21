b04770caa46389180cd35e9960c45da0dabd445b
Christian Treffs <ctreffs@gmail.com>
2020-05-21 22:44:54 +0200

    Fix SIMD

--- Sources/Bullet/SIMD+Extensions.swift
+++ Sources/Bullet/SIMD+Extensions.swift
@@ -8,31 +8,17 @@
 public typealias Vector3 = SIMD3<Double>
 public typealias Vector4 = SIMD4<Double>
 
-extension SIMD4 where Scalar == Double {
+extension Vector4 where Scalar == Double {
     public static let identity = SIMD4<Double>(x: 0, y: 0, z: 0, w: 1)
 }
 
-extension SIMD3: Sequence {
-    @inlinable
-    public __consuming func makeIterator() -> IndexingIterator<[Scalar]> {
-        [x, y, z].makeIterator()
-    }
-}
-
-extension SIMD4: Sequence {
-    @inlinable
-    public __consuming func makeIterator() -> IndexingIterator<[Scalar]> {
-        [x, y, z, w].makeIterator()
-    }
-}
-
-public protocol SIMDUnsafeScalarAccessible: SIMD {
+protocol SIMDUnsafeScalarAccessible: SIMD {
     func unsafeScalars<Result>(_ body: (UnsafePointer<Scalar>) throws -> Result) rethrows -> Result
     mutating func unsafeMutableScalars<Result>(_ body: (UnsafeMutablePointer<Scalar>) throws -> Result) rethrows -> Result
 }
 
 extension SIMD2: SIMDUnsafeScalarAccessible {
-    public func unsafeScalars<Result>(_ body: (UnsafePointer<Scalar>) throws -> Result) rethrows -> Result {
+    func unsafeScalars<Result>(_ body: (UnsafePointer<Scalar>) throws -> Result) rethrows -> Result {
         try withUnsafePointer(to: self) {
             try $0.withMemoryRebound(to: Scalar.self, capacity: 2) {
                 try body($0)
@@ -40,7 +26,7 @@ extension SIMD2: SIMDUnsafeScalarAccessible {
         }
     }
 
-    public mutating func unsafeMutableScalars<Result>(_ body: (UnsafeMutablePointer<Scalar>) throws -> Result) rethrows -> Result {
+    mutating func unsafeMutableScalars<Result>(_ body: (UnsafeMutablePointer<Scalar>) throws -> Result) rethrows -> Result {
         try withUnsafeMutablePointer(to: &self) {
             try $0.withMemoryRebound(to: Scalar.self, capacity: 2) {
                 try body($0)
@@ -50,7 +36,7 @@ extension SIMD2: SIMDUnsafeScalarAccessible {
 }
 
 extension SIMD3: SIMDUnsafeScalarAccessible {
-    public func unsafeScalars<Result>(_ body: (UnsafePointer<Scalar>) throws -> Result) rethrows -> Result {
+    func unsafeScalars<Result>(_ body: (UnsafePointer<Scalar>) throws -> Result) rethrows -> Result {
         try withUnsafePointer(to: self) {
             try $0.withMemoryRebound(to: Scalar.self, capacity: 3) {
                 try body($0)
@@ -58,7 +44,7 @@ extension SIMD3: SIMDUnsafeScalarAccessible {
         }
     }
 
-    public mutating func unsafeMutableScalars<Result>(_ body: (UnsafeMutablePointer<Scalar>) throws -> Result) rethrows -> Result {
+    mutating func unsafeMutableScalars<Result>(_ body: (UnsafeMutablePointer<Scalar>) throws -> Result) rethrows -> Result {
         try withUnsafeMutablePointer(to: &self) {
             try $0.withMemoryRebound(to: Scalar.self, capacity: 3) {
                 try body($0)
@@ -68,7 +54,7 @@ extension SIMD3: SIMDUnsafeScalarAccessible {
 }
 
 extension SIMD4: SIMDUnsafeScalarAccessible {
-    public func unsafeScalars<Result>(_ body: (UnsafePointer<Scalar>) throws -> Result) rethrows -> Result {
+    func unsafeScalars<Result>(_ body: (UnsafePointer<Scalar>) throws -> Result) rethrows -> Result {
         try withUnsafePointer(to: self) {
             try $0.withMemoryRebound(to: Scalar.self, capacity: 4) {
                 try body($0)
@@ -76,7 +62,7 @@ extension SIMD4: SIMDUnsafeScalarAccessible {
         }
     }
 
-    public mutating func unsafeMutableScalars<Result>(_ body: (UnsafeMutablePointer<Scalar>) throws -> Result) rethrows -> Result {
+    mutating func unsafeMutableScalars<Result>(_ body: (UnsafeMutablePointer<Scalar>) throws -> Result) rethrows -> Result {
         try withUnsafeMutablePointer(to: &self) {
             try $0.withMemoryRebound(to: Scalar.self, capacity: 4) {
                 try body($0)
