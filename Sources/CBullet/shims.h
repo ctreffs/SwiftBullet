53a89c21cbef740365a6045867bae4e87e2d8cc0
Christian Treffs <ctreffs@gmail.com>
2021-06-16 00:16:16 +0200

    Fix shims

--- Sources/CBullet/shims.h
+++ Sources/CBullet/shims.h
@@ -1,3 +1,4 @@
+// Bullet 3
 #define BT_ENABLE_ENET
 #define BT_ENABLE_CLSOCKET
 #define BT_ENABLE_PHYSX
@@ -5,8 +6,31 @@
 #define BT_ENABLE_MUJOCO
 
 #ifdef __APPLE__
-#include "apple.h"
-#endif
+
+// === APPLE =========
+
+#include <Bullet/SharedMemory/SharedMemoryPublic.h>
+#include <Bullet/SharedMemory/PhysicsClientC_API.h>
+#include <Bullet/SharedMemory/PhysicsClientGRPC_C_API.h>
+#include <Bullet/SharedMemory/PhysicsClientSharedMemory2_C_API.h>
+#include <Bullet/SharedMemory/PhysicsClientSharedMemory_C_API.h>
+#include <Bullet/SharedMemory/PhysicsClientTCP_C_API.h>
+#include <Bullet/SharedMemory/PhysicsClientUDP_C_API.h>
+#include <Bullet/SharedMemory/PhysicsDirectC_API.h>
+#include <Bullet/SharedMemory/PhysicsLoopBackC_API.h>
+#include <Bullet/SharedMemory/SharedMemoryInProcessPhysicsC_API.h>
+// #include <Bullet/Collision/CollisionSdkC_Api.h> // -> Must use 'struct' tag to refer to type 'lwContactPoint'
+#include <Bullet/SharedMemory/dart/DARTPhysicsC_API.h>
+#include <Bullet/SharedMemory/mujoco/MuJoCoPhysicsC_API.h>
+#include <Bullet/SharedMemory/physx/PhysXC_API.h>
+#include <Bullet/TinyAudio/b3Sound_C_Api.h>
+
 #else
-#include "linux.h"
+
+// === Linux ========
+// FIXME: No C-API yet for linux!!!
+//#include "/usr/include/bullet/btBulletCollisionCommon.h"
+//#include "/usr/include/bullet/btBulletDynamicsCommon.h"
+
+
 #endif
